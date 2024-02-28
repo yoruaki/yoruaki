533b25e71299e8bbe8d5bc0fc25d627de3215a19
Yoruaki <yoruaki@gmail.com>
2024-02-28 20:22:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# yoruaki.com
+
+This repository contains the source files for the [yoruaki.com](https://yoruaki.com/).
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+        theme: {
+            extend: {},
+        },
+        plugins: [],
+    }
+}
